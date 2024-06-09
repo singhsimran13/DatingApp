f05ccb78d1337ac7de374ed94c14e26cd7528b2a
singhsimran13 <simran.guru.71@gmail.com>
2024-06-09 16:14:26 +0530

    Streamline errors for application

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -18,7 +18,6 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe({
       next: _ => this.router.navigateByUrl('/members'), // () or _ for
-      error: err => this.toastr.error(err.error),
     })
   }
 
