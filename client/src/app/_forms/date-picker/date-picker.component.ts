233078216b74d8242f05e223f3cb5df2dfd85e9f
singhsimran13 <simran.guru.71@gmail.com>
2024-07-06 14:06:19 +0530

    Used reactive forms to register.  Now user can register using UI/UX.  Other code fixes and improvements.

---
+++ client/src/app/_forms/date-picker/date-picker.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+
+@Component({
+  selector: 'app-date-picker',
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.css']
+})
+export class DatePickerComponent implements ControlValueAccessor {
+
+  @Input() label: string = '';
+  @Input() maxDate: Date | undefined;
+  bsconfig: Partial<BsDatepickerConfig> | undefined;
+
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+    this.bsconfig = {
+      containerClass: 'theme-red',
+      dateInputFormat: 'DD MMMM YYYY',
+    }
+  }
+
+  writeValue(obj: any): void { }
+
+  registerOnChange(fn: any): void { }
+
+  registerOnTouched(fn: any): void { }
+
+  get control(): FormControl {
+    return this.ngControl.control as FormControl;
+  }
+}
